98287ea909c686a9a0b63877e802ef4b0597f6b7
Stefano Imperiale <stefano.imperiale@aizoon.it>
2020-06-26 16:51:46 +0200

    basic middleware

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, resp) => {
-    //console.log(req.url, req.method, req.headers);
-    resp.setHeader('Content-Type', 'text/html');
-    resp.write('<html lang="en">');
-    resp.write('<head><title>My first page</title></head>');
-    resp.write('<body><h1>Hello from my Nodejs Server!</h1></body>');
-    resp.write('</html>');
-    resp.end();
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
 });
 
+app.use((req, res, next) => {
+    console.log('In the another middleware');
+});
+
+const server = http.createServer(app);
+
 server.listen(3000);
- 

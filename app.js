5f26140e3f467bad1e046f39a154ab54e507d8c3
Stefano Imperiale <stefano.imperiale@aizoon.it>
2020-06-26 16:40:23 +0200

    use of Http module

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, resp) => {
+    //console.log(req.url, req.method, req.headers);
+    resp.setHeader('Content-Type', 'text/html');
+    resp.write('<html lang="en">');
+    resp.write('<head><title>My first page</title></head>');
+    resp.write('<body><h1>Hello from my Nodejs Server!</h1></body>');
+    resp.write('</html>');
+    resp.end();
+});
+
+server.listen(3000);
+ 

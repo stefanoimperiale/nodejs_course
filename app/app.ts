d5d028c5efef23ce2e274e5e746d88a4b202c1c4
Stefano Imperiale <stefano.imperiale@aizoon.it>
2020-06-27 20:06:05 +0200

    Ejs template

--- app/app.ts
+++ app/app.ts
@@ -4,17 +4,10 @@ import bodyParser from "body-parser";
 import {router as adminRoutes} from "./routes/admin"
 import shopRoutes from "./routes/shop";
 import path from "path";
-import expressHbs from "express-handlebars";
 
 const app: Express = express();
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'app/views/layouts/',
-    extname: 'hbs',
-    defaultLayout: 'main-layout'
-}));
-
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', path.join('app', 'views'));
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -24,7 +17,10 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: null
+    });
 });
 
 app.listen(3000);
